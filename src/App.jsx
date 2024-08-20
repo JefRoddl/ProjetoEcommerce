6efacf9afc4740ed62df1df6dd0ae4460f61a090
Jeferson Rodrigues <jefersonroddl@gmail.com>
2024-08-19 21:11:13 -0300

    adicionando css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
-import './App.css'
+import Catalog from './components/Catalog'
+import Cart from './components/Cart'
+import ThankYouPage from './components/ThankYouPage'
 
 function App() {
 
@@ -9,6 +11,13 @@ function App() {
       <Link to='/'>Catálogo</Link>
       <Link to='/cart'>Carrinho</Link>
     </nav>
+    <div className='container'>
+      <Routes>
+        <Route path="/" element={<Catalog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/thank-you" element={<ThankYouPage />} />
+      </Routes>
+    </div>
   </BrowserRouter>
   
 }
